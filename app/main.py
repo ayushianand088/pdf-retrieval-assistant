886060cdbf609574ba67e3ce2c706c4c99306bc9
AyushiAnand70047 <ayushianand088@gmail.com>
2025-10-02 21:03:11 +0530

    fastapi server created, file uploaded to disk

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, UploadFile
+from uuid import uuid4
+from utils.file import save_to_disk
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"status": "healthy"}
+
+@app.post("/upload")
+async def upload_file(file: UploadFile):
+    id = uuid4()
+    
+    file_path = f"/mnt/uploads/{id}/{file.filename}"
+    await save_to_disk(file=await file.read(), path=file_path)
+    
+    return {"file_id": id}
